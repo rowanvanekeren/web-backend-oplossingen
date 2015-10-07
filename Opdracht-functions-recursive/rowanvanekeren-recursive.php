d360d48bfe0176a15440fd7c569ea5ec942f7859
rowanvanekeren <rowanvanekeren@hotmail.com>
2015-10-07 10:54:35 +0200

    oplossingen toeg

--- Opdracht-functions-recursive/rowanvanekeren-recursive.php
+++ Opdracht-functions-recursive/rowanvanekeren-recursive.php
@@ -1,6 +1,24 @@
 <?php
+$geld = 100000;
+$rente = 1.08;
 
 
+
+function berekenen ( $getal){
+static $jaar = 0;
+    if($jaar < 10){
+    $jaar++;
+        echo " jaar= " . $jaar . " bedrag= " . $getal . " ";
+    $geld = berekenen($getal * 1.08);
+   
+    
+}
+
+
+
+}
+
+ berekenen($geld);
 ?>
 
 <!doctype html>
